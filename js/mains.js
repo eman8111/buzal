b0a52b49a83e9c31a2c52b5549c66fb7becbd65a
eman <eman2laa99@gmail.com>
2022-04-17 06:18:29 +0200

    buzal

---
+++ js/mains.js
@@ -0,0 +1,13 @@
+// document.querySelectorAll('a[href^="#"]').forEach(anchor =>{
+//     anchor.addEventListener("click",function(e){
+//         e.preventDefault();
+//         document.querySelector(this.getAttribute("href")).scrollInterview({
+//             behavior : "smooth"
+//         });
+//     });
+// });
+
+// var scroll = new SmoothScroll('.navbar a[href*="#"]', {
+// 	speed: 1200,
+// 	speedAsDuration: true
+// });
